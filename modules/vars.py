1345d74f100e9f226485e04c4370b1fa133ceb04
tjlords <tjlordsmobile2@gmail.com>
2025-10-06 08:08:05 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -14,7 +14,7 @@ cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 TOTAL_USER = os.environ.get('TOTAL_USERS', '741668895').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '741668895').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '741668895,-2084001270').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -23,6 +23,7 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
